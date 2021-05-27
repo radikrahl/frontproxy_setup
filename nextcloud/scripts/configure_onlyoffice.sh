1b2a3d0f3b4f816a3455120c28de54e3b45bdb8d
andreas.krahl <andreas.krahl@netzkern.de>
2021-05-27 19:13:00 +0200

    updated configure onlyoffice script; use specific version of nextcloud docker-compose file

--- nextcloud/scripts/configure_onlyoffice.sh
+++ nextcloud/scripts/configure_onlyoffice.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-
 set -x
 
-docker exec -u www-data nextcloud php occ --no-warnings config:system:set allow_local_remote_servers  --value=true
+docker exec -u www-data nextcloud php occ --no-warnings config:system:set allow_local_remote_servers --value=true
 
 docker exec -u www-data nextcloud php occ --no-warnings app:install onlyoffice
 
-# docker exec -u www-data app-server php occ --no-warnings config:system:set onlyoffice DocumentServerUrl --value="/ds-vpath/"
-# docker exec -u www-data app-server php occ --no-warnings config:system:set onlyoffice DocumentServerInternalUrl --value="http://onlyoffice-document-server/"
-# docker exec -u www-data app-server php occ --no-warnings config:system:set onlyoffice StorageUrl --value="http://nginx-server/"
+docker exec -u www-data app-server php occ --no-warnings config:system:set onlyoffice DocumentServerUrl --value="<your-office-url-here>"
+docker exec -u www-data app-server php occ --no-warnings config:system:set onlyoffice DocumentServerInternalUrl --value="http://onlyoffice-document-server/"
+docker exec -u www-data app-server php occ --no-warnings config:system:set onlyoffice StorageUrl --value="http://nextcloud/"
